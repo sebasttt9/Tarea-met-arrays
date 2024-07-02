ecf79bca0ae434385c0c3c0df015f405ea4acb62
sebasttt9 <167321213+sebasttt9@users.noreply.github.com>
2024-07-01 23:48:34 -0500

    Tarea3.js

--- Tarea3.js
+++ Tarea3.js
@@ -1,5 +1,6 @@
-/*Crear una función que calcule el promedio de un conjunto de calificaciones almacenados en un array. La función debe recibir el array y devolver únicamente la parte entera del promedio.
-const calificaciones = [79, 66, 91, 45, 77, 88]*/
+/*Crear una función que calcule el promedio de un conjunto de calificaciones almacenados en un array. 
+La función debe recibir el array y devolver únicamente la parte entera del promedio.*/
+const calificaciones = [79, 66, 91, 45, 77, 88]
 
 function calcularPromedioEntero(calificaciones) {
     const sumatoria = calificaciones.reduce((total, calificacion) => total + calificacion, 0);
